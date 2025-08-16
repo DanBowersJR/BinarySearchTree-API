81d3bedaa4b3b7d98d280a36103fdfc570a7940e
Daniel Bowers <34407755+DanBowersJR@users.noreply.github.com>
2025-08-16 03:19:56 -0230

    Initial commit: Binary Search Tree API project setup

---
+++ src/main/java/com/keyin/binarysearchtreeapi/BinarySearchTreeApiApplication.java
@@ -0,0 +1,13 @@
+package com.keyin.binarysearchtreeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BinarySearchTreeApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BinarySearchTreeApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/keyin/binarysearchtreeapi/BinarySearchTreeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keyin.binarysearchtreeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BinarySearchTreeApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
